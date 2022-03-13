b62cbb35226bbdc5ac6734046c6306bd4bdef86d
KateKatze <dr.katekogler@gmail.com>
2022-03-13 16:37:50 +0100

    diagrams done

---
+++ SQL DB and Queries/queries.sql
@@ -0,0 +1,8 @@
+--how many products were bought from a specific company
+SELECT 
+
+--who purchased products on this date
+
+--what  products were sent between this and that date, 
+
+--how many products were sent to a specific city etc. 
